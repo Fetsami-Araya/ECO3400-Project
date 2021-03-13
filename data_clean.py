539a3e7dc329a9e39fd273eccbb6efd89786a5c0
Fetsami Araya <44071164+Fetsami-Araya@users.noreply.github.com>
2021-03-12 17:59:47 -0700

    Created data_clean.py to begin combining data

---
+++ data_clean.py
@@ -0,0 +1,6 @@
+"""
+@Date: March 12, 2021
+@Authors: Fetsami Araya, Sam Kirsh, Matthew Rayka
+@Description: Creation of master dataframe containing all the economic data that are
+going to be used to train the machine learning nowcasting models.
+"""
