1b1f0a460b0b283947220a954db8bf031a233a11
Fetsami Araya <44071164+Fetsami-Araya@users.noreply.github.com>
2021-03-20 17:31:29 -0600

    Created file to begin estimating the nowcasting machine learning models

---
+++ data/nowcast_ml.py
@@ -0,0 +1,13 @@
+"""
+@Date: March 20, 2021
+@Authors: Fetsami Araya
+@Description: This file will estimate LASSO, Ridge, Elastic Net, Gradient Boosting Tree, SVM, and neural network
+machine learning models to nowcast Canadian GDP from 1960 through to 2020.
+
+In addition, the predictive performance of these models will be assessed and compared to the predicitive accuracy
+of a benchmark AR(1) model.
+"""
+
+# Imports
+import numpy as np
+import pandas as pd
