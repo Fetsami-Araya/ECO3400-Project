3b094fbb9798e8d761b33f8a88092805d1e2e7a9
Fetsami Araya <44071164+Fetsami-Araya@users.noreply.github.com>
2021-03-20 18:51:50 -0600

    Added code to estimate AR, elastic net, and gradient boosting tree models

--- nowcast_ml.py
+++ nowcast_ml.py
@@ -13,10 +13,43 @@ import numpy as np
 import pandas as pd
 from data_clean import createMasterData
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from statsmodels.tsa.ar_model import AutoReg
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import GradientBoostingRegressor
+
 def readData(dataset_type='og'):
     master_data, master_data_some_na, master_data_no_na = createMasterData()
     master_data_dict = {'og':master_data, 'some_na':master_data_some_na, 'no_na':master_data_no_na}
-    return master_data_dict[dataset_type]
+    data = master_data_dict[dataset_type]
+    return data
 
-print(readData())
+def split_data(df,start_date,end_date):
+    return df.loc[start_date:end_date]
+
+
+def ar1model(data,lag=1):
+    ar1 = AutoReg(data,lags=lag)
+    ar1_fit = ar1.fit()
+    return ar1_fit
+
+def elasticNetModel(X,y):
+    elastic = ElasticNet(random_state=42)
+    elastic_fit = elastic.fit(X,y)
+    return elastic_fit
+
+def gradientBoostingTrees(X,y):
+    gb_tree = GradientBoostingRegressor(random_state=42)
+    gb_tree_fit = gb_tree.fit(X,y)
+    return gb_tree_fit
+
+
+if __name__ == '__main__':
+    data = readData()
+    split = split_data(data,'1961-01-01','2000-01-01')
+    ar = ar1model(split['GDP'])
+    print(ar.predict())
+
 
