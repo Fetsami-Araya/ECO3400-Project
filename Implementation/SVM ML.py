a23d77e7676bc50ce0777fbd3ecc1d8332b7681e
mraykha <52611985+mraykha@users.noreply.github.com>
2021-03-24 13:15:07 -0400

    Update SVM ML.py

--- Implementation/SVM ML.py
+++ Implementation/SVM ML.py
@@ -20,26 +20,45 @@ X.head()
 y = Mdataset['Real GDP']
 
              
-#train the machine
+#split into testing and training data
 X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.30, random_state=42)
 
 len(X_train)
 len(X_test)
 
-#predict 
+
+#scale the data
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaled_X_train = scaler.fit_transform(X_train)
+scaled_X_train
+
+scaled_y_train = scaler.fit_transform(y_train)
+scaled_y_train
+
+scaled_X_test = scaler.fit_transform(X_test)
+scaled_X_test
+
+
+
+#train and predict 
 from sklearn.svm import SVR
 
 Model = SVR()
-Model.fit(X_train, y_train)
+Model.fit(scaled_X_train, scaled_y_train)
 
-y_pred = Model.predict(X_test)
+y_pred = Model.predict(scaled_X_test)
 
 
 #evaluate
 from sklearn.metrics import r2_score,mean_squared_error
 
 mse = mean_squared_error(y_test, y_pred)
-mse
+rmse = np.sqrt(mse)
+rmse
+
+
 
 Model.score(X_test, y_test)
 
