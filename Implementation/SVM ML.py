8666c1842f99a981b8626cbae05024244b1fd4b0
mraykha <52611985+mraykha@users.noreply.github.com>
2021-03-24 15:57:00 -0400

    Update SVM ML.py
    my y_pred variable is decreasing as we move forward in time... should be increasing, not sure why. I trained the first 90% of rows and tried to use the remaining 10% to predict

--- Implementation/SVM ML.py
+++ Implementation/SVM ML.py
@@ -13,43 +13,44 @@ import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
 
+#scale the data
+from sklearn.preprocessing import StandardScaler
+
+#remove the data column since this will get confused when we standardize the date
+m= Mdataset.drop(['Date'], axis='columns')
+m.head()
+
+#standardize the data and keep headers
+scaler = StandardScaler()
+q = scaler.fit_transform(m)
+q_df = pd.DataFrame(q, columns = m.columns)
+
+
 #define variables
-X = Mdataset.drop(['Real GDP','Date'], axis='columns')
+X = q_df.drop(['Real GDP'], axis='columns')
 X.head()
 
-y = Mdataset['Real GDP']
-
+y = q_df['Real GDP']
+y.head()
              
-#split into testing and training data
-X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.30, random_state=42)
+#split into testing and training data but use 90% of beginning sequential rows to train and rest to test
+X_train = X[:int(X.shape[0]*0.9)]
+X_test = X[int(X.shape[0]*0.9):]
+y_train = y[:int(X.shape[0]*0.9)]
+y_test = y[int(X.shape[0]*0.9):]
 
 len(X_train)
 len(X_test)
 
 
-#scale the data
-from sklearn.preprocessing import StandardScaler
-
-scaler = StandardScaler()
-scaled_X_train = scaler.fit_transform(X_train)
-scaled_X_train
-
-scaled_y_train = scaler.fit_transform(y_train)
-scaled_y_train
-
-scaled_X_test = scaler.fit_transform(X_test)
-scaled_X_test
-
-
-
 #train and predict 
 from sklearn.svm import SVR
 
-Model = SVR()
-Model.fit(scaled_X_train, scaled_y_train)
+Model = SVR(kernel='rbf', C=100, epsilon=0.1, gamma='scale')
+svr= Model.fit(X_train, y_train)
 
-y_pred = Model.predict(scaled_X_test)
 
+y_pred = Model.predict(X_test)
 
 #evaluate
 from sklearn.metrics import r2_score,mean_squared_error
@@ -58,7 +59,19 @@ mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 rmse
 
+Model.score(X_test,y_test)
 
 
-Model.score(X_test, y_test)
 
+### Extras ###
+# X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.30, random_state=42)
+
+# scaler = StandardScaler()
+# scaled_X_train = scaler.fit_transform(X_train)
+# scaled_X_train
+
+# scaled_y_train = scaler.fit_transform(y_train)
+# scaled_y_train
+
+# scaled_X_test = scaler.fit_transform(X_test)
+# scaled_X_test
